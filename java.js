e4ed426da4d1cb83b39e53fdc3dc2b44222ba37f
Tumelo Mohaka <tmboomer452@gmail.com>
2022-07-26 09:22:55 +0200

    init

---
+++ java.js
@@ -0,0 +1,23 @@
+/*function multiplyBy(){
+     num1:document.getElementById("firstNumber").value;
+     num2:document.getElementById("secondNumber").value;
+     document.getElementById("answer").innerHTML=num1*num2;
+}
+function divideBy(){
+    num1:document.getElementById("firstNumber").value;
+     num2:document.getElementById("secondNumber").value;
+     document.getElementById("answer").innerHTML=num1/num2;
+}*/
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("answer").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("answer").innerHTML = num1 / num2;
+}
